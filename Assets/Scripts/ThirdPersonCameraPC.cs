e81c0832dfeb6954a329db7a8f8b3d9ce0bf50b2
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-01-22 10:28:38 +0900

    22222
    22222

--- Assets/Scripts/ThirdPersonCameraPC.cs
+++ Assets/Scripts/ThirdPersonCameraPC.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ThirdPersonCamera40 : MonoBehaviour {
+public class ThirdPersonCameraPC : MonoBehaviour {
 	public Vector2 ANGLE_MIN_MAX = new Vector2 (5f, +65f);
 	public Vector3 DISTANCE_MIN_MAX = new Vector2 (3f, 15f);
 
@@ -12,6 +12,7 @@ public class ThirdPersonCamera40 : MonoBehaviour {
 
 	Transform trans;
 	float distance = 10f;
+	float distanceBack = -1f;
 	float angleX, angleY;
 	public float sensivityY = 5.0f;
 	public float sensivityX = 5.0f;
@@ -56,18 +57,47 @@ public class ThirdPersonCamera40 : MonoBehaviour {
 			distance -= wheel * sensivityW;// * Time.deltaTime;
 			distance = Mathf.Clamp (distance, DISTANCE_MIN_MAX.x, DISTANCE_MIN_MAX.y);
 		}
+
+		/*
+		 * 캐릭터 - 장애물(콜라이더)- 카메라
+		 * 캐릭터 - 카메라 - 장애물  <- (앞으로땡김)
+		 * 
+		/**/
+		Vector3 _targetPoint = (target.position + Vector3.up * 1f);
+		Vector3 _dirBack = trans.position - _targetPoint;
+		RaycastHit _hitBack;
+		if(Physics.Raycast(_targetPoint, _dirBack, out _hitBack, distance))
+		{
+			//if(_hitBack.collider.gameObject != target.gameObject)
+			{
+				if (distanceBack == -1f)
+				{
+					distanceBack = distance;
+				}
+				distance = _hitBack.distance;
+			}
+		}
+		else
+		{
+			if(distanceBack != -1f && !Physics.Raycast(_targetPoint, _dirBack, out _hitBack, distanceBack))
+			{
+				distance = distanceBack;
+				distanceBack = -1f;
+			}
+		}
 	}
 
 
 	void LateUpdate(){
 		//move and look.
-		if (bMove || target.position != targetPosOld) 
+		//if (bMove || target.position != targetPosOld) 
 		{
 			bMove = false;
 			dirQ = Quaternion.Euler (angleX, angleY, 0);
 			//Vector3 _pos = target.position + dirQ * Vector3.back * distance;
 			//trans.position = Vector3.Slerp (trans.position, _pos, Time.deltaTime * 2f);
-			trans.position = target.position - dirQ * Vector3.forward * distance;
+			trans.position = target.position + Vector3.up * 1f - dirQ * Vector3.forward * distance;
+			trans.rotation = dirQ;
 			//trans.LookAt (target.position + Vector3.up * 1f);
 		}
 		targetPosOld = target.position; 		
